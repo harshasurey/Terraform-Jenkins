000df0d270fe4026c6a42cbacd5948c6dd3debd2
harshasurey <130497480+harshasurey@users.noreply.github.com>
2023-05-03 12:00:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-yum update -y
-yum install
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo docker pull httpd
+docker images
+docker run -itd --name facebook -p "8010:80" httpd

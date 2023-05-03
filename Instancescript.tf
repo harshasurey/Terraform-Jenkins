f312534cd7cf08e3bb9dff2e9c7615e789114347
harshasurey <130497480+harshasurey@users.noreply.github.com>
2023-05-03 14:53:39 +0530

    Update Instancescript.tf

--- Instancescript.tf
+++ Instancescript.tf
@@ -71,29 +71,6 @@ resource "aws_instance" "Docker_webserver" {
    volume_size           = "8"
    delete_on_termination = true
  }
-  #user Data
-  user_data = <<EOF
-  #!/bin/sh
-export PATH=/usr/local/bin:$PATH;
-
-yum update
-yum install docker -y
-service docker start
-docker login --username="surey" --password="Surendar123@" --email="surendarsurya72@gmail.com"
-mv /root/.dockercfg /home/ec2-user/.dockercfg
-chown ec2-user:ec2-user /home/ec2-user/.dockercfg
-usermod -a -G docker ec2-user
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-chown root:docker /usr/local/bin/docker-compose
-cat <<EOF >/home/ec2-user/docker-compose.yml
-nginx:
-  image: nginx
-  ports:
-    - "80:80"
-EOF
-chown ec2-user:ec2-user /home/ec2-user/docker-compose.yml
-/usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d
   
 tags = {
     Name = "DOCKER SERVER"
